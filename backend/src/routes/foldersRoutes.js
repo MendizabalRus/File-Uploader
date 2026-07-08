8b1b3443bfd5b144867a5b58069de50b9c7aad89
Mendizabal <rusmz@Mendizabals-MacBook-Air.local>
2026-07-08 18:19:57 +0200

    fixed getFolder controller's route

--- backend/src/routes/foldersRoutes.js
+++ backend/src/routes/foldersRoutes.js
@@ -5,7 +5,7 @@ const ensureAuthenticated = require("../controllers/ensureAuth.js");
 const foldersControllers = require("../controllers/foldersControllers.js");
 
 foldersRoutes.post("/", ensureAuthenticated, foldersControllers.postCreateFolder);
-foldersRoutes.get("/", ensureAuthenticated, foldersControllers.getFolder);
+foldersRoutes.get("/:folderId", ensureAuthenticated, foldersControllers.getFolder);
 foldersRoutes.post("/update/foder:id", ensureAuthenticated, foldersControllers.postUpdateFolder);
 foldersRoutes.post("/delete/folder:id", ensureAuthenticated, foldersControllers.postDeleteFolder);
 
